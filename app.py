c23c3ade269051ac63a5f050a853e325a04ad3c1
ngweekiat <75771397+ngweekiat@users.noreply.github.com>
2024-01-25 15:15:50 +0800

    Copied code

---
+++ app.py
@@ -0,0 +1,16 @@
+import flask
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        rate = float(request.form.get("rate"))
+        return(render_template("index.html", result=(rate*-50.6)+90.2))
+    else:
+        return render_template("index.html", result="Waiting for data")
+
+if __name__ == "__main__":
+    app.run(port = 3333)
